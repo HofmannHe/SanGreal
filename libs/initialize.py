13c229ff9dd8aab45f0cede8cb04b7d8a2e4f816
HofmannHe <hawy@qq.com>
2021-03-02 14:05:16 +0800

    增加token初始化部分

---
+++ libs/initialize.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+
+import tushare as ts
+
+
+def set_token():
+    if os.getenv("TUSHARE_TOKEN") is not None:
+        ts.set_token(os.getenv("TUSHARE_TOKEN"))
+        logging.info("set_token")
+    else:
+        logging.error("need token")
+
+
+if __name__ == "__main__":
+    set_token()
